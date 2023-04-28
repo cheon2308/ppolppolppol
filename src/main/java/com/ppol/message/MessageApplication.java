25ea2d34b8581ed8c900cc7e7680562e9b76a46a
0613minjeong <0613minjeong@gmail.com>
2023-04-28 14:35:31 +0900

    feature eureka client 등록

--- src/main/java/com/ppol/message/MessageApplication.java
+++ src/main/java/com/ppol/message/MessageApplication.java
@@ -3,12 +3,14 @@ package com.ppol.message;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @EnableCaching
 @EnableJpaAuditing
+@EnableDiscoveryClient // Eureka Server에 등록
 @SpringBootApplication
 public class MessageApplication {
 
