428abd308d4c452c1fb3d548763901551185f853
JaeWon <protoss1025@naver.com>
2023-05-03 17:46:15 +0900

    Feature feign 클라이언트 및 알람 부분 설정

---
+++ src/main/java/com/ppol/personal/util/feign/AlarmFeign.java
@@ -0,0 +1,15 @@
+package com.ppol.personal.util.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ppol.personal.config.FeignClientConfig;
+import com.ppol.personal.dto.feign.AlarmRequestDto;
+
+@FeignClient(name = "AlarmFeign", url = "${feign.url.base}" + "${feign.url.alarm.service}", configuration = FeignClientConfig.class)
+public interface AlarmFeign {
+
+	@PostMapping
+	ResponseEntity<?> alarmCreate(AlarmRequestDto alarmRequestDto);
+}
