64b0a21ad05c0a1044c48adbc8651a0189b156f1
cheon2308 <cjsgusxo@gmail.com>
2023-05-04 17:55:35 +0900

    [FE]feature/articlePage

--- ppol/lib/login/homePage.dart
+++ ppol/lib/login/homePage.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:ppol/main.dart';
 
+import '../screen/articlePage.dart';
+
 class homePage extends StatelessWidget {
   const homePage({Key? key}) : super(key: key);
 
@@ -11,47 +13,44 @@ class homePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      floatingActionButtonLocation:FloatingActionButtonLocation.startFloat,
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
-      floatingActionButton:
-      SpeedDial(
-        backgroundColor: Colors.black,
-        tooltip: "메뉴",
-        direction: SpeedDialDirection.up,
-        spaceBetweenChildren: Checkbox.width,
-        animatedIcon: AnimatedIcons.home_menu,
-        childrenButtonSize: Size(70,70),
-        buttonSize: Size(60, 60),
-        children: [
-          // SpeedDialChild(
-          //   child: Icon(Icons.menu_outlined),
-          //   label: 'Menu',
-          // ),
-          SpeedDialChild(
-            child: Icon(Icons.add),
-            label: 'Upload',
-            onTap: () => {
-              print("하이1")
-            },
-          ),
-          SpeedDialChild(
-            child: Icon(Icons.message_outlined),
-            label: 'Message',
-            onTap: () => {print("하이2")},
-          ),
-          SpeedDialChild(
-            child: Icon(Icons.account_circle),
-            label: 'account',
-            onTap: () => {
-              // Navigator.pushNamed(context, '/profile')
-              // print("프로필 나와라"),
-            Navigator.push(
-            context, MaterialPageRoute(builder: (c) => ProfileScreen())),
-            },
-          ),
-        ]
-
-      ),
+      floatingActionButton: SpeedDial(
+          backgroundColor: Colors.black,
+          tooltip: "메뉴",
+          direction: SpeedDialDirection.up,
+          spaceBetweenChildren: Checkbox.width,
+          animatedIcon: AnimatedIcons.home_menu,
+          childrenButtonSize: Size(70, 70),
+          buttonSize: Size(60, 60),
+          children: [
+            SpeedDialChild(
+              child: Icon(Icons.add),
+              label: 'Upload',
+              onTap: () => {print("하이1")},
+            ),
+            SpeedDialChild(
+              child: Icon(Icons.message_outlined),
+              label: 'Message',
+              onTap: () => {print("하이2")},
+            ),
+            SpeedDialChild(
+              child: Icon(Icons.article_sharp),
+              label: 'article',
+              onTap: () => {Navigator.push(context,
+                  MaterialPageRoute(builder: (c) => ArticleScreen()))},
+            ),
+            SpeedDialChild(
+              child: Icon(Icons.account_circle),
+              label: 'account',
+              onTap: () => {
+                // Navigator.pushNamed(context, '/profile')
+                // print("프로필 나와라"),
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (c) => ProfileScreen())),
+              },
+            ),
+          ]),
     );
   }
 }
