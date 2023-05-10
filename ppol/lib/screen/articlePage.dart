afe6a24ff1097d719f00166b8c032b21b5c0a0c1
cheon2308 <cjsgusxo@gmail.com>
2023-05-10 14:41:13 +0900

    [FE]feature/posts

--- ppol/lib/screen/articlePage.dart
+++ ppol/lib/screen/articlePage.dart
@@ -1,8 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:ppol/models/articleModel.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:ppol/screen/articleDetailPage.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 Future<List<ArticleModel>> fetchArticles() async {
   final response = await http.get(
@@ -13,9 +16,10 @@ Future<List<ArticleModel>> fetchArticles() async {
     },
   );
   if (response.statusCode == 200) {
-    final List<dynamic> jsonList = jsonDecode(response.body)['data'];
+    final List<dynamic> jsonList =
+        jsonDecode(utf8.decode(response.bodyBytes))['data'];
     final List<ArticleModel> articles =
-    jsonList.map((json) => ArticleModel.fromJson(json)).toList();
+        jsonList.map((json) => ArticleModel.fromJson(json)).toList();
     return articles;
   } else {
     throw Exception('Failed to load articles');
@@ -25,14 +29,24 @@ Future<List<ArticleModel>> fetchArticles() async {
 class ArticleCard extends StatefulWidget {
   final ArticleModel article;
 
-  const ArticleCard({Key? key, required this.article}) : super(key: key);
+  const ArticleCard({
+    Key? key,
+    required this.article,
+  }) : super(key: key);
 
   @override
   _ArticleCardState createState() => _ArticleCardState();
 }
 
 class _ArticleCardState extends State<ArticleCard> {
-  int _likeCount = 0;
+  late int _likeCount;
+  int _imageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _likeCount = widget.article.likeCount;
+  }
 
   void _toggleLike() {
     setState(() {
@@ -42,14 +56,24 @@ class _ArticleCardState extends State<ArticleCard> {
 
   @override
   Widget build(BuildContext context) {
+    if (widget.article.openStatus == 'private') {
+      return Visibility(
+        visible: false,
+        child: SizedBox(),
+      );
+    }
+
     return Card(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           ListTile(
             leading: widget.article.imageList.isNotEmpty
-                ? Image.network(widget.article.imageList.first)
-                : Image.asset('assets/icon.png'),
+                ? CircleAvatar(
+                    child: CachedNetworkImage(
+                        imageUrl: widget.article.imageList.first),
+            )
+                : CircleAvatar(child: Image.asset('assets/icon.png')),
             title: Text(
               widget.article.content,
               style: TextStyle(fontWeight: FontWeight.bold),
@@ -57,15 +81,21 @@ class _ArticleCardState extends State<ArticleCard> {
             subtitle: Text(widget.article.writer.username),
           ),
           if (widget.article.imageList.isNotEmpty)
-            Image.network(
-              widget.article.imageList.first,
+            CachedNetworkImage(
+              imageUrl: widget.article.imageList.first,
               fit: BoxFit.cover,
-              height: 300,
+              height: 350,
+            )
+          else
+            Image.asset(
+              'assets/icon.png',
+              fit: BoxFit.cover,
+              height: 350,
             ),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 IconButton(
                   icon: Icon(Icons.favorite_border),
@@ -116,7 +146,7 @@ class _ArticleScreenState extends State<ArticleScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('게시물 목록'),
+        title: Text('뽈뽈뽈'),
       ),
       body: FutureBuilder<List<ArticleModel>>(
         future: _futureArticles,
@@ -137,11 +167,14 @@ class _ArticleScreenState extends State<ArticleScreen> {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => ArticleDetailScreen(article: article),
+                          builder: (context) =>
+                              ArticleDetailScreen(article: article),
                         ),
                       );
                     },
-                    child: ArticleCard(article: article),
+                    child: ArticleCard(
+                      article: article,
+                    ),
                   );
                 },
               );
