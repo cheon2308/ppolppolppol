452a8db679058652bee97979decd644dc2cbe688
0613minjeong <0613minjeong@gmail.com>
2023-05-10 09:56:27 +0900

    feat. application.yml 수정 - eureka 등록

--- src/main/java/com/ppol/group/GroupApplication.java
+++ src/main/java/com/ppol/group/GroupApplication.java
@@ -4,6 +4,7 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -11,6 +12,7 @@ import jakarta.annotation.PostConstruct;
 
 @EnableJpaAuditing
 @EnableAsync
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GroupApplication {
 
